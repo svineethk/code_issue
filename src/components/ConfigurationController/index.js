c4f9a80d7d44d62ef43164572b667926fca59360
svineethk <vineethkumar.selvam@gmail.com>
2024-07-18 05:56:40 +0000

    need error

---
+++ src/components/ConfigurationController/index.js
@@ -0,0 +1,26 @@
+// Write your code here
+import './index.css'
+import ConfigurationContext from '../../context/ConfigurationContext'
+
+const ConfigurationController = () => (
+  <ConfigurationContext.Consumer>
+    {value => {
+      const {
+        showContent,
+        showLeftNavbar,
+        showRightNavbar,
+        onToggleShowContent,
+        onToggleShowLeftNavbar,
+        onToggleShowRightNavbar,
+      } = value
+
+      return (
+        <div className="controller-container">
+          <h1>Hello</h1>
+        </div>
+      )
+    }}
+  </ConfigurationContext.Consumer>
+)
+
+export default ConfigurationController
